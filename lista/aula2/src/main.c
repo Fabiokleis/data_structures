8b4f0256b251b0772d476ea57d9a289d6dd67c29
fabiokleis <fabiohkrc@gmail.com>
2021-11-10 19:44:53 -0300

    feito a implementacao de uma lista duplamente encadeada e as funcoes propostas dos exercicios 1 e 2

--- lista/aula2/src/main.c
+++ lista/aula2/src/main.c
@@ -3,16 +3,52 @@
 /*Função que insere o valor -1 antes da primeira ocorrência de {elemento}
  *ou caso {elemento} não exista, a lista deve permanecer inalterada! */
 List* inserir_antes (List *lista, int elemento) {
+    List* aux = buscar(lista, elemento);
 
-   /*TERMINAR!!!*/  	
+    if (aux != NULL) {
+        // novo nó
+        List *no = (List *)malloc(sizeof(List));
+        no->info = -1;
+        no->next = aux;
+        if (aux->prev != NULL) {
+            aux->prev->next = no;
+        }
+        no->prev = aux->prev;
+        aux->prev = no;
 
+        while(aux->prev != NULL) {
+            aux = aux->prev;
+        }
+
+        lista = aux;
+    }
+
+    return lista;
 }
 
 /*Função que insere o valor -1 depois da primeira ocorrência de {elemento}
  *ou caso {elemento} não exista, a lista deve permanecer inalterada! */
 List* inserir_depois (List *lista, int elemento) {
-   
-   /*TERMINAR!!!*/  	
+    List* aux = buscar(lista, elemento);
+
+    if (aux != NULL) {
+        // novo nó
+        List *no = (List *)malloc(sizeof(List));
+        no->info = -1;
+        no->prev = aux;
+        if (aux->next != NULL) {
+            aux->next->prev = no;
+        }
+        no->next = aux->next;
+        aux->next = no;
+        while(aux->prev != NULL) {
+            aux = aux->prev;
+        }
+
+        lista = aux;
+    }
+
+    return lista;
 
 }
 
@@ -29,7 +65,7 @@ int main () {
    }   
 
    imprimir (lista1);
-
+   
    lista1 = inserir_antes (lista1, 4);
    lista1 = inserir_antes (lista1, 10);
    lista1 = inserir_antes (lista1, 0);
@@ -38,11 +74,14 @@ int main () {
 
    imprimir (lista2);
 
+   
+   
    lista2 = inserir_depois (lista2, 4);
    lista2 = inserir_depois (lista2, 10);
    lista2 = inserir_depois (lista2, 0);
 
    imprimir (lista2);
+   
 
    return 0;
 }
