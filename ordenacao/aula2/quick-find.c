d3b8a0c028cedd2e8e368670348302bec2b3750a
fabiokleis <fabiohkrc@gmail.com>
2021-12-06 13:53:19 -0300

    corrigido uma falta no rand(), e feito o quick-find

--- ordenacao/aula2/quick-find.c
+++ ordenacao/aula2/quick-find.c
@@ -9,7 +9,41 @@ void Imprimir (int *vetor, int tamanho) {
   } 
   printf ("\n");
 }
+
+int Particione(int *vetor, int e, int d) {
+    int t;
+    int p = vetor[d]; // p é o pivô
+    int i = e - 1;
+    int j;
+
+    for (j = e; j <= (d - 1); j++) {
+        if (vetor[j] <= p) {
+            i = i + 1;
+            t = vetor[i];
+            vetor[i] = vetor[j];
+            vetor[j] = t;
+        }
+    }
+
+    t = vetor[i + 1];
+    vetor[i + 1] = vetor[d];
+    vetor[d] = t;
+
+    return i + 1;
+}
+
 int QuickFind (int *vetor, int esq, int dir, int k) {
+    if (esq <= dir) {
+        int q = Particione(vetor, esq, dir);
+        if (q == k) {
+            printf("O elemento procurado na posição %d é %d\n", k, vetor[k]);
+            return vetor[k];
+        } else if(q > k) {
+            QuickFind(vetor, esq, dir-1, k);
+        } else {
+            QuickFind(vetor, q+1, dir, k);
+        }
+    }
 }
 
 /*Função principal.*/
@@ -19,14 +53,11 @@ int main (int argc, char *argv[]) {
    srand(time(NULL));
    int i;
    
-   //int tamanho = 10;
-   //int *vetor = (int *)malloc(tamanho * sizeof(int));
-   //for (i = 0; i < tamanho; i++) {
-   //   vetor[i] = rand() % (tamanho+1);
-   //}
-   
-   int tamanho = 8;
-   int vetor[] = {7, 1, 3, 10, 17, 2, 21, 9};
+   int tamanho = 10;
+   int *vetor = (int *)malloc(tamanho * sizeof(int));
+   for (i = 0; i < tamanho; i++) {
+      vetor[i] = rand() % (tamanho+1);
+   }
    
    Imprimir (vetor, tamanho);
    start = clock();
@@ -34,6 +65,5 @@ int main (int argc, char *argv[]) {
    printf("O elemento procurado na posição %d é %d\n", pos, QuickFind(vetor, 0, tamanho-1, pos));
    end = clock();
    elapsed_time = (end - start)/(double)CLOCKS_PER_SEC;
-   //free (vetor);
    return 0;
 }
